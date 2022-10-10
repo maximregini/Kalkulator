ab8399f333cd5cd92bb0f19e3c098030d46e9ddf
MaximRegini <maxadenk@mail.ru>
2022-10-10 18:25:56 +0300

    Исправленное тестовое задание "Калькулятор"

--- src/Main.java
+++ src/Main.java
@@ -1,123 +1,217 @@
-import java.util.Scanner;
-import java.util.TreeMap;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите выражение");
+        String input = scanner.nextLine();
+        String answer =  calc(input);
+        System.out.println(answer);
+    }
+    public static String calc(String input) {
+        String str1 = input.replaceAll(" ", "");
+        int index = -1 ;
+        char action1 = getTypeOfAction(str1);
+        if (action1 == '_') {
+            try {
+                throw new IOException();
+            } catch (IOException e){
+                System.out.println("Ошибка: неизвестная операция");
+                System.exit(0);
+            }
 
-    static int Arithmetic (String operation, int a, int b) throws IOException {
-        return switch (operation) {
-            case ("+") -> a + b;
-            case ("-") -> a - b;
-            case ("*") -> a * b;
-            case ("/") -> a / b;
-            default -> throw new IOException("Неверная операция");
-        };
-    }
-     static int RomanToArabic (String number) {
-        String ArabNumber = number.toUpperCase();
-        return switch (ArabNumber) {
-            case ("I") -> 1;
-            case ("II") -> 2;
-            case ("III") -> 3;
-            case ("IV") -> 4;
-            case ("V") -> 5;
-            case ("VI") -> 6;
-            case ("VII") -> 7;
-            case ("VIII") -> 8;
-            case ("IX") -> 9;
-            case ("X") -> 10;
-            default -> -1;
-        };
-    }
-    static String ArabicToRoman (int i) {
-        StringBuilder RomeNumber = new StringBuilder();
-        var map = new HashMap<Integer, String>();
-        map.put(1, "I");
-        map.put(2, "II");
-        map.put(3, "III");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(6, "VI");
-        map.put(7, "VII");
-        map.put(8, "VIII");
-        map.put(9, "IX");
-        map.put(10, "X");
-        var sortedMap = new TreeMap<>(map);
-        while (i > 0) {
-            int in = 0;
-            var out = "";
-            for (HashMap.Entry<Integer,String> entry: sortedMap.entrySet()){
-                Integer tmp = entry.getKey();
-                if (tmp > i){
-                    i -= in;
-                    RomeNumber.append(out);
-                    break;
-                } else if (tmp == i) {
-                    i -= tmp;
-                    RomeNumber.append(entry.getValue());
-                    break;
-                } else {
-                    in = entry.getKey();
-                    out = entry.getValue();
-                }
+        } else {
+            index = str1.indexOf(action1);
+        }
+
+        String[] romanNums = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+        String[] arabicNums = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        int variable1 = 1;
+        int variable2 = 1;
+        boolean rom = false;//индекс использования римских цифр
+        String variable1str = str1.substring(0, index);
+        for (int i = 0; i < romanNums.length; i++) {
+            String variableForCompare = romanNums[i];
+            if (variable1str.equalsIgnoreCase(variableForCompare)) {
+                variable1 = i + 1;
+                rom = true;
             }
         }
-        return RomeNumber.toString();
-    }
-     static String calc(String input) throws IOException, NumberFormatException {
-        String output;
-        int x = 0;
-        int y = 0;
-        String[] subStr;
-        subStr = input.split(" ");
-        int n1 = RomanToArabic(subStr[0]);
-        if (n1 == -1) {
+        for (int i = 0; i < arabicNums.length; i++) {
+            String variableForCompare = arabicNums[i];
+            if (variable1str.equalsIgnoreCase(variableForCompare)) {
+                variable1 = i + 1;
+                rom = false;
+            }
+        }
+        if (variable1 == -1) {
             try {
-                n1 = Integer.parseInt(subStr[0]);
-                if (n1 > 10) {
-                    throw  new IOException("Неверное значение 1-го аргумента (больше 10)");
-                }
-                if (n1 < 1) {
-                    throw  new IOException("Неверное значение 1-го аргумента (меньше 1)");
-                }
-            } catch (NumberFormatException e) {
-                throw new NumberFormatException("Неверное значение 1-го аргумента (не число)");
+                throw new IOException();
+            } catch (IOException e){
+                System.out.println("Ошибка: Первый операнд больше 10");
+                System.exit(0);
             }
-            x = 1;
         }
-        int n2 = RomanToArabic(subStr[2]);
-        if (n2 == -1) {
+        String variable2str = str1.substring(index + 1);
+        for (int i = 0; i < romanNums.length; i++) {
+            String variableForCompare = romanNums[i];
+            if (variable2str.equalsIgnoreCase(variableForCompare)) {
+                if (!rom ) {
+                    try {
+                        throw new IOException();
+                    } catch (IOException e){
+                        System.out.println("Ошибка: Значения вводимых чисел должны быть однотипными");
+                        System.exit(0);
+                    }
+                }
+                variable2 = i + 1;
+            }
+        }
+        for (int i = 0; i < arabicNums.length; i++) {
+            String variableForCompare = arabicNums[i];
+            if (variable2str.equalsIgnoreCase(variableForCompare)) {
+                if (rom) {
+                    try {
+                        throw new IOException();
+                    } catch (IOException e){
+                        System.out.println("Ошибка: Значения вводимых чисел должны быть однотипными");
+                        System.exit(0);
+                    }
+                }
+                variable2 = i + 1;
+            }
+        }
+        if (variable2 == -1) {
             try {
-                n2 = Integer.parseInt(subStr[2]);
-                if (n2 > 10) {
-                    throw  new IOException("Неверное значение 2-го аргумента (больше 10)");
-                }
-                if (n2 < 1) {
-                    throw  new IOException("Неверное значение 2-го аргумента (меньше 1)");
-                }
-            } catch (NumberFormatException e) {
-                throw new NumberFormatException("Неверное значение 2-го аргумента (не число)");
+                throw new IOException();
+            } catch (IOException e){
+                System.out.println("Ошибка: Второй операнд больше 10");
+                System.exit(0);
             }
-            y = 1;
         }
-        if (x == y) {
-            int result = Arithmetic(subStr[1],n1,n2);
-            if (x == 1) {
-                output = Integer.toString(result) ;
+
+        int arabResult = getArabResult(variable1, variable2, action1);
+
+        String result = "";
+        if (rom && arabResult > 0) {
+            String romResult = IntegerToRomanNumeral(arabResult);
+            result = "Ответ: " + romResult;
+        } else if (!rom) {
+            result = "Ответ: " + arabResult;
+        } else if (arabResult < 0) {
+            try {
+                throw new IOException();
+            } catch (IOException e){
+                System.out.println("Ошибка: В римской системе не существует отрицательных числе");
+                System.exit(0);
             }
-            else {
-                if (result > 10) {
-                    throw new NumberFormatException("Ошибка");}
-                output = ArabicToRoman(result);
-            }
-        } else throw new IOException("Ошибка");
-        return output;
+        }
+        return result;
+
     }
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        System.out.println(calc(input));
+    public static char getTypeOfAction(String str1) {
+        int index = str1.indexOf("+");
+        char act1 = '_';
+        if (index > -1) {
+            act1 = '+';
+        }
+        if (index == -1) {
+            index = str1.indexOf("-");
+            if (index > -1) {
+                act1 = '-';
+            }
+        }
+        if (index == -1) {
+            index = str1.indexOf("/");
+            if (index > -1) {
+                act1 = '/';
+            }
+        }
+        if (index == -1) {
+            index = str1.indexOf("*");
+            if (index > -1) {
+                act1 = '*';
+            }
+        }
+        return act1;
+    }
+    public static int getArabResult(int variable1, int variable2, char action1) {
+        int rsl = 0;
+        if (action1 == '+') {
+            rsl = variable1 + variable2;
+        }
+        if (action1 == '-') {
+            rsl = variable1 - variable2;
+        }
+        if (action1 == '*') {
+            rsl = variable1 * variable2;
+        }
+        if (action1 == '/') {
+            if (variable2 == 0) {
+                System.out.println("Оops! Divide by zero");
+                System.exit(0);
+            }
+            rsl = (variable1 - (variable1 % variable2)) / variable2;
+        }
+        return rsl;
+    }
+    public static String IntegerToRomanNumeral(int input) {
+        if (input < 1 || input > 3999)
+            return "Invalid Roman Number Value";
+        StringBuilder s = new StringBuilder(" ");
+        while (input >= 1000) {
+            s.append("M");
+            input -= 1000;
+        }
+        while (input >= 900) {
+            s.append("CM");
+            input -= 900;
+        }
+        while (input >= 500) {
+            s.append("D");
+            input -= 500;
+        }
+        while (input >= 400) {
+            s.append("CD");
+            input -= 400;
+        }
+        while (input >= 100) {
+            s.append("C");
+            input -= 100;
+        }
+        while (input >= 90) {
+            s.append("XC");
+            input -= 90;
+        }
+        while (input >= 50) {
+            s.append("L");
+            input -= 50;
+        }
+        while (input >= 40) {
+            s.append("XL");
+            input -= 40;
+        }
+        while (input >= 10) {
+            s.append("X");
+            input -= 10;
+        }
+        while (input == 9) {
+            s.append("IX");
+            input -= 9;
+        }
+        while (input >= 5) {
+            s.append("V");
+            input -= 5;
+        }
+        while (input == 4) {
+            s.append("IV");
+            input -= 4;
+        }
+        while (input >= 1) {
+            s.append("I");
+            input -= 1;
+        }
+        return s.toString();
     }
 }
-
